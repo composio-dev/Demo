f7fa59867031664766128bf62ed8713c965d5f33
Karthikeya Meesala <71187953+Karthikeya-Meesala@users.noreply.github.com>
2025-09-22 21:56:22 +0530

    test: create new file via mercury action

---
+++ test-mercury-new-file.py
@@ -0,0 +1,4 @@
+# This is a new file created by Mercury action
+print("Hello from a new file!")
+print("No SHA needed for new files")
+print("The action handles both create and update cases automatically")
